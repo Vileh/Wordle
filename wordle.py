5935e5a64bf9b3faca8aa9c8044f353f5288da54
Viktor Penchev <victorpenchev277@gmail.com>
2023-10-15 11:47:37 +0100

    Clean up code a bit

--- wordle.py
+++ wordle.py
@@ -2,25 +2,25 @@ import os
 import random
 import pandas as pd
 import msvcrt
-from readchar import readchar
 
-
-al_data = pd.read_csv('allowed_words.txt')
-pos_data = pd.read_csv('possible_words.txt')
-allowed_data = al_data.values.tolist()
-possible_data = pos_data.values.tolist()
+# Setting up the possible words
+allowed_data = pd.read_csv('allowed_words.txt').values.tolist()
+possible_data = pd.read_csv('possible_words.txt').values.tolist()
 al = [allowed_data[i][0] for i in range(len(allowed_data))]
 pos = [possible_data[i][0] for i in range(len(possible_data))]
 all_words = al + pos
 alpha = 'abcdefghijklmnopqrstuvwxyz'
+
+# Characters for keyboard
 qwerty = ['qwertyuiop', 'asdfghjkl', 'zxcvbnm']
 
+# Set up coloured text in the terminal
 os.system('color')
 
 
 def check(guess, x):
     ret = [0, 0, 0, 0, 0]
-    # 1-bull, 2-cow
+    # 1-exact, 2-wrong pos, 0-wrong
     for i in range(5):
         for j in range(5):
             if guess[i] == x[j] and ret[i] != 1:
@@ -32,43 +32,52 @@ def check(guess, x):
 
 
 def inputs():
-    tr = ''
-    inp = ''
+    to_try = ''
     while 1:
-        ins = len(tr)
+        num_letters = len(to_try)
 
+        # Print the input line
         print('\r| ', end="")
         for i in range(5):
-            if i < ins:
-                print(tr[i].capitalize(), end=" ")
+            if i < num_letters:
+                print(to_try[i].capitalize(), end=" ")
             else:
                 print('_', end=" ")
         
+        # I'm not sure why the code even works, but it does
         print('\r| ', end="")
-        if ins != 0:
-            for i in range(ins):
-                print(tr[i].capitalize(), end=" ")
+        if num_letters != 0:
+            for i in range(num_letters):
+                print(to_try[i].capitalize(), end=" ")
         
-        inp = msvcrt.getch()
-        inp_let = str(inp)[2]
-        let = 0
+        input_raw = msvcrt.getch()
+        input = str(input_raw)[2]
+
+        # Check if input is allowed
+        allow = 0
         for i in alpha:
-            if inp_let == i:
-                let = 1
-        if let == 1 and len(tr)<5:
-            tr += inp_let
-        if inp == b'\r' and len(tr) == 5:
+            if input == i:
+                allow = 1
+
+        # Append to try if allowed
+        if allow == 1 and len(to_try)<5:
+            to_try += input
+
+        # Check if input is enter
+        if input_raw == b'\r' and len(to_try) == 5:
             for j in all_words:
-                if tr == j:
-                    return tr
+                if to_try == j:
+                    return to_try
             print("-Try another word-", end="")
-        if inp == b'\x08':
-            tr = tr[:-1]
+        
+        # Check if input is backspace
+        if input_raw == b'\x08':
+            to_try = to_try[:-1]
             print(' '*18, end="")
 
 def print_keyboard(keyboard):
     print()
-    spaces = [6, 7, 8]
+    spaces = [6, 7, 9]
     for i in range(3):
         print(' '*spaces[i], end="")
         for j in qwerty[i]:
@@ -85,9 +94,11 @@ def print_keyboard(keyboard):
 
 
 def game():
-    x = pos[random.randrange(len(pos))]
-    chck = [0, 0, 0, 0, 0, 0]
+    word_to_guess = pos[random.randrange(len(pos))]
+    checks = [0, 0, 0, 0, 0, 0]
     keyboard = {f'{x}' : 0 for x in alpha}
+
+    # Print the border and the keyboard
     print('-'*32)
     print('|' + ' '*6 + 'Welcome to Wordle!' + ' '*6 +'|')
     print('|' + ' '*2 + 'Guess the word in 6 tries!' + ' '*2 + '|') 
@@ -98,39 +109,47 @@ def game():
     print_keyboard(keyboard)
 
     for i in range(6):
+        # Move cursor up if first guess
         if i == 0:
             print("\033[F"*12 , end="")
+        
+        # Get the guess and check it
         guess = inputs()
-        end = i
-        chck[i] = check(guess.lower(), x)
-        print('\r', end="")
-        print('| ', end="")
+        current_guess = i
+        checks[i] = check(guess.lower(), word_to_guess)
+
+        # Print the guess with colours
+        print('\r| ', end="")
         for j in range(5):
-            if chck[i][j] == 0:
+            if checks[i][j] == 0:
                 print(guess[j].capitalize(), end=" ")
                 keyboard[f'{guess[j]}'] = -1
-            elif chck[i][j] == 1:
+            elif checks[i][j] == 1:
                 print('\033[92m'+guess[j].capitalize()+'\033[0m', end=" ")
                 keyboard[f'{guess[j]}'] = 1
             else:
                 print('\033[93m' + guess[j].capitalize() + '\033[0m', end=" ")
                 keyboard[f'{guess[j]}'] = 2
 
+        # Print necessary elements
         print(' '*4 + f'Guess No.{i+1}' +' '*5 + '|', end="")
         print('                         ')
-
         print('\n'*(11-i), end="")
         print_keyboard(keyboard)
         print('\033[F'*(11 -i), end="")
 
-        if chck[end] == [1, 1, 1, 1, 1]:
+        # Check if the word is guessed
+        if checks[current_guess] == [1, 1, 1, 1, 1]:
             break
     
-    if chck[end] == [1, 1, 1, 1, 1]:
+    # Print the result
+    if checks[current_guess] == [1, 1, 1, 1, 1]:
         print('\n|' + ' '*4 + 'Congrats, you got it!')
     else:
         print('\n|' + ' '*9 + 'Not quite :(')
-    print('|' +' '*5 +'The word was: ' + x.capitalize())
+    print('|' +' '*5 +'The word was: ' + word_to_guess.capitalize())
+
+    # Ask if the user wants to play again
     print('\n|' + ' '*7 + '--Again?--(y/n)', end="")
     while True:
         again = str(msvcrt.getch())[2]
